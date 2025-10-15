37898215c197cdc7eced3d1acda6b52434245b17
Dat Duy <datahihi1100@gmail.com>
2025-10-15 20:47:51 +0700

    Separated core into a composer library

--- src/Application/Drive/cache/Memcached.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Craft\Application\Drive\Cache;
-
-class Memcached
-{
-
-}
--- src/Database/Exception/UnsupportedDesignException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Craft\Database\Exception;
-
-/**
- * Exception thrown when an unsupported design pattern is requested.
- * 
- * @see \BadMethodCallException
- */
-class UnsupportedDesignException extends \BadMethodCallException {}
